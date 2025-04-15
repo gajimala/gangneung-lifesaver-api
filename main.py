f31de48e66b974d61920c81f8c155089df3e80df
gajimala <gajimala@gmail.com>
2025-04-15 22:41:30 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
+import json
+
+app = FastAPI()
+
+with open("gangneung_lifesavers.json", "r", encoding="utf-8") as f:
+    lifesavers = json.load(f)
+
+@app.get("/")
+def read_root():
+    return {"message": "Lifesaver API is running!"}
+
+@app.get("/lifesavers")
+def get_lifesavers():
+    return JSONResponse(content=lifesavers)
+
+app.mount("/public", StaticFiles(directory="public"), name="public")
