cf2ec06589aed68addfec00ab1339445f8441c11
gajimala <gajimala@gmail.com>
2025-04-15 22:42:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from fastapi.staticfiles import StaticFiles
+import os
 import json
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-with open("gangneung_lifesavers.json", "r", encoding="utf-8") as f:
-    lifesavers = json.load(f)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-def read_root():
-    return {"message": "Lifesaver API is running!"}
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+JSON_PATH = os.path.join(BASE_DIR, "gangneung_lifesavers.json")
+
+with open(JSON_PATH, "r", encoding="utf-8") as f:
+    lifesavers = json.load(f)
 
 @app.get("/lifesavers")
 def get_lifesavers():
-    return JSONResponse(content=lifesavers)
-
-app.mount("/public", StaticFiles(directory="public"), name="public")
+    return lifesavers
