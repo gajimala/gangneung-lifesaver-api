1e7e213ef9b0eb8e1b7521c1b087de4e135ea599
gajimala <gajimala@gmail.com>
2025-04-17 00:50:13 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles  # ✅ 추가!
+from fastapi.staticfiles import StaticFiles
 import json
 
 app = FastAPI()
 
-# ✅ 정적 파일 라우팅 설정
+# 정적 파일 서비스
 app.mount("/", StaticFiles(directory="public", html=True), name="static")
 
 # CORS 설정
@@ -17,16 +17,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# JSON 데이터 로드
+# lifesaver 데이터 로드
 with open("gangneung_lifesavers.json", "r", encoding="utf-8") as f:
     lifesavers = json.load(f)
 
-# 기본 라우트
 @app.get("/api")
 def read_root():
     return {"message": "인명구조함 API 동작 중"}
 
-# 인명구조함 위치 반환
 @app.get("/lifesavers")
 def get_lifesavers():
     return lifesavers
