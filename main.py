8a1a5c673c6a94b6f35d683cc9e2c5a0af4bcdf7
gajimala <gajimala@gmail.com>
2025-04-16 23:04:19 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import json
 
 app = FastAPI()
 
-# CORS 설정 (모든 도메인 허용)
+# CORS 설정
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -13,11 +14,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# 데이터 로드
-with open("gangneung_lifesavers.json", "r", encoding="utf-8") as f:
+# JSON 데이터 로드
+with open("gangneung_lifesavers.json", encoding="utf-8") as f:
     lifesavers = json.load(f)
 
-# 라우터 설정
+@app.get("/")
+def root():
+    return {"message": "Hello, this is the lifesaver API!"}
+
 @app.get("/lifesavers")
-def read_lifesavers():
+def get_lifesavers():
     return lifesavers
