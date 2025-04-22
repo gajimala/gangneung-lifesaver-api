4f5d35a45a97ceeb5499cfc31fde110cafb3890f
gajimala <gajimala@gmail.com>
2025-04-22 22:31:09 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 import json
 import os
 
@@ -14,9 +15,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# 정적 파일 서빙 경로 등록
+app.mount("/static", StaticFiles(directory="public"), name="static")
+
 # JSON 데이터 로드
 DATA_FILE = os.path.join(os.path.dirname(__file__), "nationwide_lifesavers_coordinates_only.json")
-
 with open(DATA_FILE, "r", encoding="utf-8") as f:
     lifesavers = json.load(f)
 
