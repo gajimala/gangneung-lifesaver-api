def3a8f7ae84642ca5570922ea11f4edf15bb6e0
gajimala <gajimala@gmail.com>
2025-04-24 01:27:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,14 +18,17 @@ app.add_middleware(
 # 정적 파일 서빙 경로 등록
 app.mount("/", StaticFiles(directory="public", html=True), name="static")
 
-# JSON 데이터 로드
-DATA_FILE = os.path.join(os.path.dirname(__file__), "nationwide_lifesavers_coordinates_only.json")
-with open(DATA_FILE, "r", encoding="utf-8") as f:
+# lifesaver 데이터 로드
+with open("gangneung_lifesavers.json", "r", encoding="utf-8") as f:
     lifesavers = json.load(f)
 
-@app.get("/nationwide_lifesavers_coordinates_only")
-def get_nationwide_lifesavers_coordinates_only():
-    return nationwide_lifesavers_coordinates_only
+@app.get("/api")
+def read_root():
+    return {"message": "인명구조함 API 동작 중"}
+
+@app.get("/lifesavers")
+def get_lifesavers():
+    return lifesavers
 
 # HTML 파일 제공 (퍼블릭 폴더 내 파일)
 @app.get("/lifesaver-map-naver")
