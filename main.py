74ec32e02ef27000541aae1ffdeea34700e7ddec
gajimala <gajimala@gmail.com>
2025-04-26 12:38:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app.add_middleware(
 app.mount("/", StaticFiles(directory="public", html=True), name="static")
 
 # JSON 데이터 로드
-DATA_FILE = os.path.join(os.path.dirname(__file__), "nationwide_lifesavers_coordinates_only.json")
+DATA_FILE = os.path.join(os.path.dirname(__file__), "lifesavers.json")
 with open(DATA_FILE, "r", encoding="utf-8") as f:
     lifesavers = json.load(f)
 
